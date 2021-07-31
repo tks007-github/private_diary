91acbb96f52ca968b4161b0b8f27974192a89a32
tks007-github <takeshi.n.2020@gmail.com>
2021-07-31 16:27:45 +0900

    update

--- private_diary/urls.py
+++ private_diary/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.contrib import path, include
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('diary.urls')),
 ]
