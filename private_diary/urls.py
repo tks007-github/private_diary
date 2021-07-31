946a3919c9546a58e837c1db029fa1e3a33b0dbc
tks007-github <takeshi.n.2020@gmail.com>
2021-07-31 16:39:50 +0900

    update

--- private_diary/urls.py
+++ private_diary/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from django.contrib import admin
-from django.contrib import path, include
+from django.urls import path, include
 
 
 urlpatterns = [
