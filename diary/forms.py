7e889300ac09499255bb8038d5cd07d10968267c
tks007-github <takeshi.n.2020@gmail.com>
2021-08-09 17:22:09 +0900

    update

--- diary/forms.py
+++ diary/forms.py
@@ -5,6 +5,7 @@ class InquiryForm(forms.Form):
     name = forms.CharField(label='お名前', max_length=30)
     email = forms.EmailField(label='メールアドレス')
     title = forms.CharField(label='タイトル', widget=forms.Textarea)
+    message = forms.CharField(label='メッセージ', widget=forms.Textarea)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
